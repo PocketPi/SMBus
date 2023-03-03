62b28da8ec540c9da3f0a99ebff6d7115d813088
Peter Rasmussen <peter@soundboks.com>
2023-03-03 11:50:09 +0100

    more warnings

--- example/main.c
+++ example/main.c
@@ -5,17 +5,20 @@
 #define ADDR 0x12
 
 void i2c_transmit(uint8_t i2c_addr, uint8_t *tx_data, uint8_t count) {
+    (void)i2c_addr;
     printf("tx_data: ");
     for (size_t i = 0; i < count; i++) {
         printf("0x%02x ", tx_data[i]);
     }
     printf("\n\r");
-};
+}
+
 void i2c_recieve(uint8_t i2c_addr, uint8_t *rx_data, uint8_t count) {
-    for (size_t i = 0; i < count; i++) {
+    (void)i2c_addr;
+    for (uint8_t i = 0; i < count; i++) {
         rx_data[i] = i;
     }
-};
+}
 
 int main(void) {
     uint16_t value = smbus_read_word(ADDR, 0x0D);
--- smbus.c
+++ smbus.c
@@ -13,13 +13,13 @@ uint16_t smbus_read_word(uint8_t i2c_addr, uint8_t command_code) {
     i2c_recieve(i2c_addr, rx_data, 2);
 
     return rx_data[1] << 8 | rx_data[0];
-};
+}
 
 void smbus_write_word(uint8_t i2c_addr, uint8_t command_code, uint16_t data) {
-    uint8_t tx_data[3] = {command_code, data >> 8, data & 0xff};
+    uint8_t tx_data[3] = {command_code, (uint8_t)(data >> 8), (uint8_t)(data & 0xff)};
 
     i2c_transmit(i2c_addr, tx_data, sizeof(tx_data));
-};
+}
 
 void smbus_block_read(uint8_t i2c_addr, uint8_t command_code) {
     uint8_t tx_data[1] = {command_code};
@@ -27,7 +27,7 @@ void smbus_block_read(uint8_t i2c_addr, uint8_t command_code) {
 
     i2c_transmit(i2c_addr, tx_data, sizeof(tx_data));
     i2c_recieve(i2c_addr, rx_data, SMBUS_MAX_READ_SIZE);
-};
+}
 
 void smbus_block_write(uint8_t i2c_addr, uint8_t command_code, uint8_t *data, uint8_t count) {
     uint8_t tx_data[256] = {command_code, count};
@@ -40,7 +40,7 @@ void smbus_block_write(uint8_t i2c_addr, uint8_t command_code, uint8_t *data, ui
 
     // Transmit count bytes + 2 for `command_code` and `count`
     i2c_transmit(i2c_addr, tx_data, count + 2);
-};
+}
 
 uint8_t *get_rx_data() {
     return rx_data;
